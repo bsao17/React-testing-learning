3b1f2196a41c2f6b8c5244b0b88527693d1ef0c7
bsao17 <declic62@gmail.com>
2022-02-17 08:00:25 +0100

    Add new tests

--- src/component/title/index.test.js
+++ src/component/title/index.test.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import Title from "./Title.js";
 
+// testing view
 describe("should test Title component", ()=>{
    //test state value
    beforeEach(()=>{
@@ -12,6 +13,21 @@ describe("should test Title component", ()=>{
         const title = document.querySelector("h1");
         //assert title
         expect(title).toHaveTextContent("ma boutique de drone");
+    });
+    
+    it("should render search input", ()=>{
+        //get search input
+        const search = document.querySelector("input");
+        //assert search input
+        expect(search).toHaveAttribute("placeholder", "rechercher un drone");
+    });
+
+    // testing form
+    it("should render form", ()=>{
+        //get form
+        const form = document.querySelector("form");
+        //assert form
+        expect(form).toBeInTheDocument();
     });
 });
 
