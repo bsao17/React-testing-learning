5430cbb4fa935242f0b192e3700ee01bd281306a
bsao17 <declic62@gmail.com>
2022-02-18 13:52:07 +0100

    add request fake API

--- src/component/root-page/title/Title.js
+++ src/component/root-page/title/Title.js
@@ -1,17 +1,37 @@
-import React, {useState} from "react"
-import style from "./title.module.scss"
+import React, { useEffect, useState } from "react";
+import style from "./title.module.scss";
 
+export default function Title() {
+  const [title] = useState("ma boutique de drones");
+  const [data, setData] = useState(null);
 
-export default function Title (){
-    const [title] = useState("ma boutique de drones");
-    return(
-        <div id="title">
-            <div id={"drone-title"}>{title}</div>
-            <img id="img" src={"image/dronePresentation.png"} alt="test React picture" width={200} height={200}/>
-            <form>
-                <input type="text" placeholder="rechercher un drone" />
-                <button type="submit">Search</button>
-            </form>
-        </div>
-    )
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div id="title">
+      <div id={"drone-title"}>{title}</div>
+      <img
+        id="img"
+        src={"image/dronePresentation.png"}
+        alt="test React picture"
+        width={200}
+        height={200}
+      />
+      <form>
+        <input type="text" placeholder="rechercher un drone" />
+        <button type="submit">Search</button>
+      </form>
+      {data &&
+        data.map((product) => {
+          console.log(product);
+          return <div id={style.products} key={product.id}>{product.title} : {product.price}</div>;
+        })}
+    </div>
+  );
 }
