70eda049a51204ccf73eeb06b1d55704e4cc1096
bsao17 <declic62@gmail.com>
2022-02-18 20:57:04 +0100

    mock implement test

--- src/component/root-page/requestAPI/request.test.js
+++ src/component/root-page/requestAPI/request.test.js
@@ -1,14 +1,23 @@
 import { render } from "@testing-library/react";
 import RequestAPI from "./RequestAPI";
 
-describe("async query test", ()=>{
-    beforeEach(()=>{
-        render(<RequestAPI/>)
-    })
-    
-    it("should test API query", ()=>{
-        function callback(data){
-            expect(data).toBe("hello world")
-        }
-    })
-})
+describe("async query test", () => {
+  beforeEach(() => {
+    render(<RequestAPI />);
+  });
+
+  it("should test API query", async () => {
+    // test with mock
+    const mock = jest.fn();
+    mock.mockResolvedValueOnce({
+      data: [
+        {
+          id: 1,
+          title: "drone 1",
+          price: "100",
+          image: "image/drone1.png",
+        },
+      ],
+    });
+  });
+});
